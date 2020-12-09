33ddb38350ccf7248723471ef8e7ba81bbebcd79
Chris Bertrand <cbertrand78@outlook.com>
2020-12-09 14:27:09 +0000

    First Test
    The first test has been written and then implemented.  Technically the previous commit shouldn't have happened because you don't commit broken code.  But I was just illustrating the procedure.

---
+++ DrinksMachine.Logic/WaterCommand.cs
@@ -0,0 +1,15 @@
+﻿using DrinksMachine.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrinksMachine.Logic
+{
+    public class WaterCommand : IDrinkCommand
+    {
+        public CommandResult PerformCommand(Drink drink)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
