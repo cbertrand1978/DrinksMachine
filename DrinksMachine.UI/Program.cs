d73b53ba1ef29811b94f5b633eb9b7c73dda891f
Chris Bertrand <cbertrand78@outlook.com>
2020-12-09 12:42:02 +0000

    Initial Commit
    Committing basic solution structure and dependencies.

---
+++ DrinksMachine.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrinksMachine.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
